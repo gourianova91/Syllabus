0274e286f8a7d733ca3c948dbe2183cb076a9c8b
Anastaciya <gourianova91@gmail.com>
2018-09-13 10:54:29 +0300

    add Yandex’s AShot library to take screenshots after tests run

--- core/src/main/java/listeners/TestListener.java
+++ core/src/main/java/listeners/TestListener.java
@@ -5,30 +5,64 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.*;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 
+import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 
 public class TestListener implements ITestListener, ISuiteListener, IInvokedMethodListener
 {
-    String filePath = "C:\\Projects\\Test\\Syllabus\\screenshots";
+    //Current Directory
+    private String currentDir = System.getProperty("user.dir");
+
+    //GetScreenShot Method Directory and Image File
+    private File getSreenShotMethodImageFile = new File (currentDir +
+            "\\ScreenShots\\GetScreenShotMethod\\amazonscreenshot.png");
+
+    //Element Screenshot Directory and Image File
+    private File webElementImageFile = new File(currentDir +
+            "\\ScreenShots\\Ashot\\WebElement\\logo.png" );
+
+    //Entirepage ScreenShot Directory and Image File
+    private File entirePageImageFile = new File(currentDir +
+            "\\ScreenShots\\Ashot\\EntirePage\\entirepage.png" );
+
+    @Override
+    public void onTestFailure(ITestResult result){
+        System.out.println("***** Error "+result.getName()+" test has failed *****");
+        String methodName=result.getName().toString().trim();
+        takeScreenShot(methodName);
+    }
+
+    public void takeScreenShot(String methodName) {
+        //Screenshot viewable area////////////////
+        //get the driver
+       /* File scrFile = ((TakesScreenshot) Driver.getInstance().getDriver()).getScreenshotAs(OutputType.FILE);
+        //The below method will save the screen shot in drive with test method name
+        try {
+            //FileUtils.copyFile(scrFile, getSreenShotMethodImageFile);
+            FileUtils.copyFile(scrFile, new File(currentDir + "\\ScreenShots\\GetScreenShotMethod\\"+methodName+".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        ///////////////////////////////////////
+        //Take Screenshot of entire page by AShot
+        Screenshot entirePageScreenShot = new AShot().
+                shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(Driver.getInstance().getDriver());
+        //Write Screenshot to a file
+        try {
+        ImageIO.write(entirePageScreenShot.getImage(),"PNG", new File(currentDir + "\\ScreenShots\\Ashot\\EntirePage\\"+methodName+".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
         //
-    }
-
-    public void takeScreenShot(String methodName) {
-        //get the driver
-        File scrFile = ((TakesScreenshot) Driver.getInstance().getDriver()).getScreenshotAs(OutputType.FILE);
-        //The below method will save the screen shot in drive with test method name
-        try {
-            FileUtils.copyFile(scrFile, new File(filePath+methodName+".png"));
-            System.out.println("***Placed screen shot in "+filePath+" ***");
-        } catch (IOException e) {
-            System.out.println("=======================================================================");
-            e.printStackTrace();
-        }
     }
 
     @Override
@@ -42,15 +76,10 @@ public class TestListener implements ITestListener, ISuiteListener, IInvokedMeth
     }
 
     @Override
-    public void onTestFailure(ITestResult result){
-        System.out.println("***** Error "+result.getName()+" test has failed *****");
+    public void onTestSuccess(ITestResult result){
+        System.out.println("***** Success "+result.getName()+" test is OK *****");
         String methodName=result.getName().toString().trim();
         takeScreenShot(methodName);
-    }
-
-    @Override
-    public void onTestSuccess(ITestResult var1){
-        //
     }
 
     @Override
