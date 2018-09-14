52f9d063fb401c0d4dd4159bd2b2741d45d8462c
Anastaciya <gourianova91@gmail.com>
2018-09-14 11:15:32 +0300

    create new class for allure attachments

---
+++ core/src/main/java/listeners/AllureAttachments.java
@@ -0,0 +1,67 @@
+package listeners;
+
+import driver.Driver;
+import io.qameta.allure.Attachment;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+
+import javax.imageio.ImageIO;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class AllureAttachments {
+
+    @Attachment(value = "Page")
+    public static byte[] takeScreenShot()
+    {
+        /*Screenshot viewable area by AShot*/
+        Screenshot entirePageScreenShot = new AShot().takeScreenshot(Driver.getInstance().getDriver());
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(entirePageScreenShot.getImage(),"PNG", baos);
+            baos.flush();
+            byte[] imageInByte = baos.toByteArray();
+            baos.close();
+            return imageInByte;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "Unable to Get Screenshot.".getBytes();
+    }
+
+    //@Attachment(value="EntirePage Screenshot of {0}", type="image/png")
+    public static byte[] takeScreenShotEntirePage(String name) {
+        /*Take Screenshot of entire page by AShot*/
+        Screenshot entirePageScreenShot = new AShot().
+                shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(Driver.getInstance().getDriver());
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(entirePageScreenShot.getImage(),"PNG", baos);
+            baos.flush();
+            byte[] imageInByte = baos.toByteArray();
+            baos.close();
+            return imageInByte;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "Unable to Get Screenshot.".getBytes();
+    }
+
+    /*To Convert the File to Bytes*/
+    private static byte[] fileToBytes(String fileName) throws Exception
+    {
+        File file = new File(fileName);
+        return Files.readAllBytes(Paths.get(file.getAbsolutePath()));
+    }
+
+    /*To Attach the PNG Image File to the Allure Report*/
+    @Attachment(value="PNG Attachment", type = "image/png")
+    public static byte[] attachFileType_PNG(String filePath) throws Exception
+    {
+        return fileToBytes(filePath);
+    }
+}
--- core/src/main/java/listeners/TestListener.java
+++ core/src/main/java/listeners/TestListener.java
@@ -21,113 +21,83 @@ public class TestListener implements ITestListener, ISuiteListener, IInvokedMeth
     //Current Directory
     private static String currentDir = System.getProperty("user.dir");
 
-    //GetScreenShot Method Directory and Image File
-    private File getSreenShotMethodImageFile = new File (currentDir +
-            "\\ScreenShots\\GetScreenShotMethod\\amazonscreenshot.png");
-
-    //Element Screenshot Directory and Image File
-    private File webElementImageFile = new File(currentDir +
-            "\\ScreenShots\\Ashot\\WebElement\\logo.png" );
-
-    //Entirepage ScreenShot Directory and Image File
-    private File entirePageImageFile = new File(currentDir +
-            "\\ScreenShots\\Ashot\\EntirePage\\entirepage.png" );
-
-    @Override
-    public void onTestFailure(ITestResult result){
-        System.out.println("***** Error "+result.getName()+" test has failed *****");
-        String methodName=result.getName().toString().trim();
-        if (Driver.isDriverRunning()) {
-            Allure.getLifecycle().addAttachment(methodName, "image/png", "png", takeScreenShot());
-            //takeScreenShot(methodName);
-        }
-    }
-
-    //@Attachment(value="EntirePage Screenshot of {0}", type="image/png")
-    public static byte[] takeScreenShot() {
-        //////////Screenshot viewable area////////////////
-
-        //get the driver
-        /*File scrFile = ((TakesScreenshot) Driver.getInstance().getDriver()).getScreenshotAs(OutputType.FILE);
-        //The below method will save the screen shot in drive with test method name
-        try {
-            //FileUtils.copyFile(scrFile, getSreenShotMethodImageFile);
-            FileUtils.copyFile(scrFile, new File(currentDir + "\\ScreenShots\\GetScreenShotMethod\\"+methodName+".png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-
-        //////////////////////////////////////////////
-
-        //Take Screenshot of entire page by AShot
-        Screenshot entirePageScreenShot = new AShot().
-                shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(Driver.getInstance().getDriver());
-        //Write Screenshot to a file
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        try {
-            ImageIO.write(entirePageScreenShot.getImage(),"PNG", baos/*new File(currentDir + "\\ScreenShots\\Ashot\\EntirePage\\"+methodName+".png")*/);
-            baos.flush();
-            byte[] imageInByte = baos.toByteArray();
-            baos.close();
-            return imageInByte;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "Unable to Get Screenshot.".getBytes();
-        //////////////////////////////////////////////
-    }
-
-    @Override
-    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-        //
-    }
-
-    @Override
-    public void onTestStart(ITestResult var1){
-        //
-    }
-
-    @Override
-    public void onTestSkipped(ITestResult result) {
-        //
-    }
-
-    @Override
-    public void onTestSuccess(ITestResult result){
-        System.out.println("***** Success "+result.getName()+" test is OK *****");
-        String methodName=result.getName().toString().trim();
-        if (Driver.isDriverRunning()) {
-            Allure.getLifecycle().addAttachment(methodName, "image/png", "png", takeScreenShot());
-            //takeScreenShot(methodName);
-        }
-    }
-
-    @Override
-    public void onStart(ITestContext var1){
-        //
-    }
-
-    @Override
-    public void onFinish(ITestContext var1){
-        //
-    }
-
+    /*This belongs to ISuiteListener and will execute before the Suite Starts*/
     @Override
     public void onStart(ISuite arg0) {
-        //
+        Reporter.log("Execution of the Suite '" + arg0.getName() + "' Started!", false);
+    }
+
+    /*This belongs to ISuiteListener and will execute after the Suite Ends*/
+    @Override
+    public void onFinish(ISuite arg0) {
+        Reporter.log("Execution of the Suite '" + arg0.getName() + "' Completed!", false);
+    }
+
+    /*This belongs to ITestListener, It will execute at the time of Test Execution */
+    @Override
+    public void onStart(ITestContext var1){
+        Reporter.log("About to begin executing Test " + var1.getName(), false);
+    }
+
+    /*This belongs to ITestListener, It will execute at the End of Test*/
+    @Override
+    public void onFinish(ITestContext var1){
+        Reporter.log("Completed executing test " + var1.getName(), false);
+    }
+
+    /*This belongs to ITestListener, It will Execute only when the Test is PASSED*/
+    @Override
+    public void onTestSuccess(ITestResult result){
+        Reporter.log("\nSUCCESFULLY EXECUTED TEST: " + result.getTestClass().getName() + "." + result.getMethod().getMethodName() + "\n", true);Reporter.log("\n");
+        String Name = result.getTestClass().getName() + "." + result.getMethod().getMethodName() ;
+        if (Driver.isDriverRunning()) {
+            //Allure.getLifecycle().addAttachment(Name, "image/png", "png", AllureAttachments.takeScreenShotEntirePage(Name));
+            AllureAttachments.takeScreenShot();
+        }
+    }
+
+    /*This belongs to ITestListener, It will Execute only when the Test is FAILED*/
+    @Override
+    public void onTestFailure(ITestResult result){
+        Reporter.log("\nFAILED TEST: " + result.getTestClass().getName() + "." + result.getMethod().getMethodName() + "\n", true);
+        String Name = result.getTestClass().getName() + "." + result.getMethod().getMethodName() ;
+        if (Driver.isDriverRunning()) {
+            //Allure.getLifecycle().addAttachment(methodName, "image/png", "png", takeScreenShot());
+            AllureAttachments.takeScreenShot();
+        }
+    }
+
+    /*This belongs to ITestListener, It and will execute before the Main Test Starts (@Test)*/
+    @Override
+    public void onTestStart(ITestResult result){
+        Reporter.log("\nSTARTED TESTING: " + result.getTestClass().getName() + "." + result.getMethod().getMethodName() + "\n", true);
+    }
+
+    /*This belongs to ITestListener, It will execute only if any of the Main Test(@Test) Gets Skipped*/
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        Reporter.log("\nSKIPPED TEST: " + result.getTestClass().getName() + "." + result.getMethod().getMethodName() + "\n", true);
+    }
+
+    /*This belongs to IInvokedMethodListener, It will execute before every method Including @Before @After @Test*/
+    public void beforeInvocation(IInvokedMethod arg0, ITestResult arg1) {
+        String textMsg = "About to begin executing following method : " + returnMethodName(arg0.getTestMethod());
+        Reporter.log(textMsg, false);
+    }
+
+    /*This belongs to IInvokedMethodListener and will execute after every method Including @Before @After @Test*/
+    public void afterInvocation(IInvokedMethod arg0, ITestResult arg1) {
+        String textMsg = "Completed executing following method : " + returnMethodName(arg0.getTestMethod());
+        Reporter.log(textMsg, false);
+    }
+
+    /*This will return method names to the calling function*/
+    private String returnMethodName(ITestNGMethod method)
+    {
+        return method.getRealClass().getSimpleName() + "." + method.getMethodName();
     }
 
     @Override
-    public void onFinish(ISuite arg0) {
-        //
-    }
-
-    public void beforeInvocation(IInvokedMethod arg0, ITestResult arg1) {
-        //
-    }
-
-    public void afterInvocation(IInvokedMethod arg0, ITestResult arg1) {
-        //
-    }
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) { }
 
 }
